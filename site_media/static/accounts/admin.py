bcb3204817b8e73f4e68ac74b808a718b2403a5c
Flavio Curella <flavio.curella@gmail.com>
2012-01-18 15:18:07 -0600

    removed static files from site_media/static. Those should be moved the by the collectstatic command

--- site_media/static/accounts/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import UserProfile, RedmineAccount
-
-admin.site.register(UserProfile)
-admin.site.register(RedmineAccount)
--- site_media/static/accounts/forms.py
+++
@@ -1,8 +0,0 @@
-from .models import RedmineAccount
-from django.forms import ModelForm
-
-
-class RedmineAccountForm(ModelForm):
-    class Meta:
-        model = RedmineAccount
-        exclude = ('user',)
